5ff445b768afef1cffb4260b6c0b67b4c2ff57bc
signoutz <aunkaffpom@gmail.com>
2023-10-04 14:39:13 +0700

    asd

--- application/third_party/PHPExcel/WorksheetIterator.php
+++ application/third_party/PHPExcel/WorksheetIterator.php
@@ -25,7 +25,6 @@
  * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
  * @version    ##VERSION##, ##DATE##
  */
-#[\ReturnTypeWillChange] 
 class PHPExcel_WorksheetIterator implements Iterator
 {
     /**
