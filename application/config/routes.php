5917e83e656692e431d3b89138cd5cb2db8faa8d
signoutz <aunkaffpom@gmail.com>
2023-11-21 21:06:17 +0700

    sd

--- application/config/routes.php
+++ application/config/routes.php
@@ -84,7 +84,8 @@ $route['visitors'] 	= 'main/ga';
 $route['map-visualization'] 	= 'main/mapvz';
 $route['pm25-map-visualization'] 	= 'main/mapvz2';
 
-$route['2ndDustBoyDay'] = 'main/dustboyday';
+$route['2ndDustBoyDay'] = 'main/dbday';
+
 $route['download_excel/(:any)'] = 'main/download_excel';
 $route['download_excel2/(:any)'] = 'main/download_excel2';
 $route['download_json/(:any)']  = 'main/download_json';
