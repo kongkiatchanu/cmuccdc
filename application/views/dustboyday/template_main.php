c8fb9695ebc4f3791396c315758dd7ebecfd4039
signoutz <aunkaffpom@gmail.com>
2023-11-22 11:13:49 +0700

    asd

--- application/views/dustboyday/template_main.php
+++ application/views/dustboyday/template_main.php
@@ -179,7 +179,7 @@
                                 </div>
 
                                 <div class="ms-4">
-                                    <h3>Wellcome message</h3>
+                                    <h3>Welcome message</h3>
 
                                     <p class="mb-0">a happy moment What is CCDC</p>
                                 </div>
