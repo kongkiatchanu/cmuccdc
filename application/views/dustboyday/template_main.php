3483dfbd94c65ff2794e70582c87d5d47845e802
signoutz <aunkaffpom@gmail.com>
2023-11-22 09:56:51 +0700

    s

--- application/views/dustboyday/template_main.php
+++ application/views/dustboyday/template_main.php
@@ -155,11 +155,7 @@ https://templatemo.com/tm-583-festava-live
             </div>
 
             <div class="video-wrap">
-                <!-- <video autoplay="" loop="" muted="" class="custom-video" poster=""> -->
-                    <!-- <source src="https://www.youtube.com/embed/R3j6OruIJvE?si=6J29-B9I4aXttrmB" type="video/mp4"> -->
-                    <iframe class="custom-video" src="https://www.youtube.com/embed/R3j6OruIJvE?si=6J29-B9I4aXttrmB?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <!-- Your browser does not support the video tag. -->
-                <!-- </video> -->
+                <iframe class="custom-video" src="https://www.youtube-nocookie.com/embed/R3j6OruIJvE?si=3m7f-CHWipG2-Lw4?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </section>
 
