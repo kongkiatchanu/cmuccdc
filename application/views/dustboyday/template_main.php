960d224aa515f2e35bb1f392586a484e40ee627f
signoutz <aunkaffpom@gmail.com>
2023-11-30 16:47:54 +0700

    asd

--- application/views/dustboyday/template_main.php
+++ application/views/dustboyday/template_main.php
@@ -159,7 +159,12 @@
                                 </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse show  d-lg-block" aria-labelledby="headingOne" data-bs-parent="#myTabContent">
                                 <div class="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    <table class="table">
+                                        <tr>
+                                            <td>8.30</td>
+                                            <td>Openning Remark</td>
+                                        </tr>
+                                    </table>
                                 </div>
                                 </div>
 
